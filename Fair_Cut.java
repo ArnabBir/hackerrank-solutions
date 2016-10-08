b114e0950e8212fb949767635c4c9e0f5dedb693
Arnab Bir <arnabbir@gmail.com>
2016-10-08 18:06:44 +0530

    Update Fair_Cut.java

--- Fair_Cut.java
+++ Fair_Cut.java
@@ -1,18 +1,24 @@
 import java.io.*;
 import java.util.*;
-
+import java.lang.Math;
 public class Solution {
     
     public static long maxF(int [] A, int n, int k){
-        long unfairness = 0;
-        for(int i = 0; i < k; ++i){
-            for(int j = k; j < n; ++j){
-                unfairness += Math.abs(A[i] - A[j]);
+        long [][]unfairness = new long [n + 1][n + 1];
+        long Li, Lu;
+        for(int i = 0; i < n + 1; ++i) Arrays.fill(unfairness[i], Long.MAX_VALUE);
+        unfairness[0][0] = 0;
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j <= i; ++j){
+                if(j > k && i - j > n - k) continue;
+                Li = unfairness[i][j] + A[i] * ((i - j) - ((n - k) - (i -j)));
+                Lu = unfairness[i][j] + A[i] * (j - (k - j));
+                if(unfairness[i + 1][j + 1] > Li) unfairness[i + 1][j + 1] = Li;
+                if(unfairness[i + 1][j] > Lu) unfairness[i + 1][j] = Lu;
             }
         }
-        return unfairness;
+        return unfairness[n][k];
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -22,6 +28,7 @@ public class Solution {
             A[i] = sc.nextInt();
         }
         Arrays.sort(A);
-        System.out.println(maxF(A, n, k));
+        
+        System.out.println(maxF(A, n, ((k <= n / 2) ? k : n - k)));
     }
 }
