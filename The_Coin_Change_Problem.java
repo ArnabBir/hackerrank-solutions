f8bee440a3723087d2aaf7604a2d4af67f1d37bd
Arnab Bir <arnabbir@gmail.com>
2016-10-13 11:27:18 +0530

    Create The_Coin_Change_Problem.java

---
+++ The_Coin_Change_Problem.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static int countCombinations(int [] C, int N){
+        if(N == 0) return 0;
+        for(int i = 0; i < C.length; ++i){
+            if(N >= C[i]) return 1 + countCombinations(C, N - C[i]);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int [] C = new int[M];
+        for(int i = 0; i < M; ++i) C[i] = sc.nextInt();
+        System.out.println(countCombinations(C,N));
+    }
+}
