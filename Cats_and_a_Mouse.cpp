3fa785f803ef6669a1a42f3a52bc0579e57bb5cd
Arnab Bir <arnabbir@gmail.com>
2017-10-03 20:39:15 +0530

    Create Cats_and_a_Mouse.cpp

---
+++ Cats_and_a_Mouse.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++){
+        int x;
+        int y;
+        int z;
+        cin >> x >> y >> z;
+        cout<<(abs(x-z) > abs(y-z) ? "Cat B" : (abs(x-z) < abs(y-z) ? "Cat A" : "Mouse C"))<<endl;
+    }
+    return 0;
+}
