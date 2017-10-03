eb6d1758ffe81be7073c1552d34928942707e03a
Arnab Bir <arnabbir@gmail.com>
2017-10-03 20:32:55 +0530

    Create The_Hurdle_Race.cpp

---
+++ The_Hurdle_Race.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> height(n);
+    for(int height_i = 0; height_i < n; height_i++){
+       cin >> height[height_i];
+    }
+    int maxH = *max_element(height.begin(), height.end());
+    cout<<max(0, maxH-k)<<endl;
+    return 0;
+}
